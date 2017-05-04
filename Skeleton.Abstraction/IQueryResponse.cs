74e444c5d22c2ce4e7c91642a85d35f7c936d863
Jérôme Quiles <jej666@gmail.com>
2017-05-04 20:57:41 +0200

    Add files via upload

---
+++ Skeleton.Abstraction/IQueryResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Skeleton.Abstraction
+{
+    //Move links to generic metadata dictionary
+    public interface IQueryResponse<T>
+    {
+        string NextPageLink { get; set; }
+
+        string PrevPageLink { get; set; }
+
+        IEnumerable<T> Results { get; set; }
+
+        int TotalCount { get; set; }
+
+        int TotalPages { get; set; }
+    }
+}
