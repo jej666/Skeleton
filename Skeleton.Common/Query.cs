5199ae87626ae9a2f3a79709334363f18e847470
Jérôme Quiles <jej666@gmail.com>
2017-05-04 20:59:26 +0200

    Add files via upload

---
+++ Skeleton.Common/Query.cs
@@ -0,0 +1,15 @@
+﻿using Skeleton.Abstraction.Orm;
+
+namespace Skeleton.Common
+{
+    public class Query : IQuery
+    {
+        public virtual string Fields { get; set; }
+      
+        public virtual string OrderBy { get; set; }
+
+        public virtual int? PageNumber { get; set; }
+
+        public virtual int? PageSize { get; set; }
+    }
+}
