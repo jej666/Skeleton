20ac61872a8d59f2b1bccbfbe556aa1d794b5f0a
Jérôme Quiles <jej666@gmail.com>
2017-05-04 20:57:16 +0200

    Add files via upload

---
+++ Skeleton.Abstraction/Orm/IQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace Skeleton.Abstraction.Orm
+{
+    //Commit forced
+    public interface IQuery
+    {
+        int? PageNumber { get; set; }
+
+        int? PageSize { get; set; }
+
+        string OrderBy { get; set; }
+
+        string Fields { get; set; }
+    }
+}
