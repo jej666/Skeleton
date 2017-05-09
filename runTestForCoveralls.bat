ad2548fbd51ea4fe1f9752ea575b388de15080f0
Jérôme Quiles <jej666@gmail.com>
2017-05-09 18:03:21 +0200

    Update runTestForCoveralls.bat

--- runTestForCoveralls.bat
+++ runTestForCoveralls.bat
@@ -1,8 +1,10 @@
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
-nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
+nuget install coveralls.io -Version 1.3.4 -OutputDirectory tools
 
 .\tools\OpenCover.4.6.519\OpenCover.Console.exe 
 -register:user -filter:"+[*]*" 
 -target:"packages\NUnit.ConsoleRunner.3.6.1\tools\nunit-console.exe" 
 -targetargs:"/noshadow /domain:single  Skeleton.Tests.Infrastructure\bin\debug\Skeleton.Tests.Infrastrucuture.dll" 
 -output:coverage.xml
+
+.\tools\coveralls.io.1.3.4\tools\coveralls.net.exe --opencover coverage.xml
